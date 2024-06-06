88a951b21d60986b0e2d3f43cb13f63bfb8b0442
Proyecto-D <haroldafonso@gmail.com>
2024-06-05 21:19:50 -0500

    Update inFacebook.js

--- Controllers/inFacebook.js
+++ Controllers/inFacebook.js
@@ -1,16 +1,18 @@
 import { loginFacebook, providerFacebook } from '../Controllers/firebase.js'
 
-const facebook = document.getElementById('facebook')
+const facebook = document.getElementById('facebookR')
 
 async function logFacebook() {
   try {
     await loginFacebook(providerFacebook)
     window.location.href = '../templates/home.html'
   } catch (error) {
-    console.log(error)
+    console.error('Error al iniciar sesión con Facebook:', error)
+    alert('Error al iniciar sesión con Facebook. Inténtelo de nuevo.')
   }
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  facebook.addEventListener('click', logFacebook)
+facebook.addEventListener('click', (e) => {
+  e.preventDefault()
+  logFacebook()
 })
